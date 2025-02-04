d30dbf1f6f5989163d67b2f7aa821629e857edf2
Vinita Hingonekar <hingonekarvinita@gmail.com>
2025-02-04 15:43:25 +0530

    Starting off with Linked List

--- Linked-List-Snake/include/LinkedList/Node.h
+++ Linked-List-Snake/include/LinkedList/Node.h
@@ -1,8 +1,11 @@
 #pragma once
-#include "BodyPart.h"
+#include "../Player/BodyPart.h"
 
-namespace Player
+
+namespace LinkedList
 {
+	using namespace Player;
+
 	struct Node
 	{
 		BodyPart body_part;
