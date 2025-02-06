4a4db817e3242c66eff52c4e3eebe6e06c3a19be
Vinita Hingonekar <hingonekarvinita@gmail.com>
2025-02-07 03:18:46 +0530

    Create Double Linked List

---
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -0,0 +1,29 @@
+#include "LinkedListLib/DoubleLinked/DoubleLinkedList.h"
+
+namespace LinkedListLib
+{
+	namespace DoubleLinked
+	{
+		DoubleLinkedList::DoubleLinkedList() = default;
+		DoubleLinkedList::~DoubleLinkedList() = default;
+
+
+		void DoubleLinked::shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node)
+		{
+
+		}
+
+		void DoubleLinked::removeNodeAtIndex(int index)
+		{
+
+		}
+
+		void DoubleLinked::shiftNodesAfterRemoval(Node* cur_node)
+		{
+
+		}
+
+	}
+}
+
+
