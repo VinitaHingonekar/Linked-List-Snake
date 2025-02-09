b3c2f56705568dc249e3aaf87c8483d196964c4c
Vinita Hingonekar <hingonekarvinita@gmail.com>
2025-02-09 13:44:25 +0530

    Delete at Beginning - DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -123,6 +123,22 @@ namespace LinkedListLib
 
 		}
 
+		void DoubleLinkedList::removeNodeAtHead() 
+		{
+			linked_list_size--;
+
+			Node* cur_node = head_node;
+			head_node = head_node->next;
+
+			if (head_node != nullptr)
+			{
+				static_cast<DoubleNode*>(head_node)->previous = nullptr;
+			}
+
+			cur_node->next = nullptr;
+			delete cur_node;
+		}
+
 		void DoubleLinkedList::removeNodeAtIndex(int index)
 		{
 
